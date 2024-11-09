2b9d2ad5223ec04af53bd5fd761ae15a33a73826
TomasEGliozzo <tegliozzo@gmail.com>
2024-11-09 19:43:18 -0300

    add: preparación main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,23 @@
+import Colegio.Aula
+import Colegio.Colegio
+import Curso.Asistencia
+import Curso.Boletin
+import Curso.Curso
+import Curso.Examen
+import Curso.Horario
+import Curso.Materia
+import Curso.Nota
+import Materiales.Libro
+import Materiales.Notebook
+import Materiales.Stock
+import Persona.Directivo
+import Persona.Estudiante
+import Persona.PadreTutor
+import Persona.Persona
+import Persona.PersonalAdministrativo
+import Persona.Preceptor
+import Persona.Profesor
+
 def main():
     return 0
 
